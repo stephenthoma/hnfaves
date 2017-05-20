9555830e5b3f98dbaedbed3e3d922b4d6aa7da82
sthoma <sthoma4@gmail.com>
2017-05-20 11:34:43 -0600

    Fix index overlap for more items.

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@
     if ( httpRequest === undefined ) {
       return;
     }
-    var count = document.getElementById('favorites-list').getElementsByTagName('li').length - 1;
+    var count = document.getElementById('favorites-list').getElementsByTagName('li').length;
     httpRequest.onreadystatechange = renderItems;
     httpRequest.open( 'GET', `http://localhost:8001/more?count=${count}` );
     httpRequest.send();
@@ -19,13 +19,13 @@
 
   function renderItems() {
     if ( httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200 ) {
-      var listItem = ({ url, title, numFavoriters, commentId, numComments }) => `
+      var listItem = ({ url, title, numFavoriters, itemId, numComments }) => `
         <li class="favorites-list-item">
           <a class="item-title" href=${url}> ${title}</a>
           <div class="item-meta">
             <span class="item-meta-favoriters"> Favorited by ${numFavoriters}</span>
             <span class="item-meta-bullet"> &bull;</span>
-            <a class="item-meta-comments" href="https://news.ycombinator.com/item?id=${commentId}"> ${numComments} comments</a>
+            <a class="item-meta-comments" href="https://news.ycombinator.com/item?id=${itemId}"> ${numComments} comments</a>
           </div>
         </li>
       `;
