093fe25f4e588ea063b6cdc236bd26e82aaf6ed8
sthoma <sthoma4@gmail.com>
2017-05-30 17:58:56 -0600

    Add comments + reorder.

--- util/favoriteCrawler.js
+++ util/favoriteCrawler.js
@@ -3,50 +3,28 @@ const request = require('./request.js');
 const API_BASE_URL ='https://hacker-news.firebaseio.com/v0/';
 
 module.exports = {
-    getTopPosts,
-    getItemCounts,
-    getFavorites,
+    traversePost,
     getPostInfo,
-    traversePost
+    getFavorites,
+    getItem,
+    getTopPosts
 };
 
-function getTopPosts( numPosts ) {
-    return new Promise( ( resolve, reject ) => {
-        request( `${API_BASE_URL}topstories.json` ).then( ( response ) => {
-            resolve( JSON.parse( response ).slice( 0, numPosts ) );
-        }).catch( ( error ) => reject( error ) );
-    });
-}
-
-function getItem( type, itemID ) {
-    return new Promise( ( resolve, reject ) => {
-        request( `${API_BASE_URL}${type}/${itemID}.json` ).then( ( response ) => {
-            resolve( JSON.parse( response ) );
-        }).catch( ( error ) => reject( error ) );
-    });
-}
-
-function getFavorites( userID ) {
-    return new Promise( ( resolve, reject ) => {
-        let promiseArray = [];
-        for ( let page in [...Array(2).keys()] ) {
-            promiseArray.push( request( `https://news.ycombinator.com/favorites?id=${userID}&p=${+page + 1}` ) );
+function traversePost( postId, queue, visited, callback ) {
+    getPostInfo( postId ).then( ( post ) => {
+        visited[ post.id ] = post;
+        queue.push.apply( queue, post.kids );
+    }).catch( (error ) => {
+        console.log( error );
+    }).then( () => {
+        if( queue.length === 0 ) {
+            callback( visited );
+        } else {
+            let nextPostId = queue.shift();
+            traversePost( nextPostId, queue, visited, callback );
         }
-        Promise.all( promiseArray ).then( pages => {
-            let favorites = pages.map( ( page ) => {
-                const STORY_REGEX = /<tr class='athing' id='([0-9]+)'/g;
-                let matchArr;
-                let pageFavorites = [];
-                while ( ( matchArr = STORY_REGEX.exec( page ) ) !== null ) {
-                    pageFavorites.push( matchArr[1] );
-                }
-                return pageFavorites;
-            });
-            resolve( [].concat.apply( [], favorites ) );
-        }).catch( ( error ) => reject( error ) );
     });
 }
-
 function getPostInfo( postID ) {
     return new Promise( ( resolve, reject ) => {
         try {
@@ -60,7 +38,7 @@ function getPostInfo( postID ) {
                     }
                     resolve( item );
                 } else {
-                    if ( resItem.deleted === true ) {
+                    if ( resItem.deleted === true ) { // Better way of handling this case?
                         item.id = -1;
                         item.kids = [];
                         item.by = 'deleted';
@@ -76,19 +54,41 @@ function getPostInfo( postID ) {
     });
 }
 
-function traversePost( postId, queue, visited, callback ) {
-    getPostInfo( postId ).then( ( post ) => {
-        visited[ post.id ] = post;
-        queue.push.apply( queue, post.kids );
-    }).catch( (error ) => {
-        console.log( error );
-    }).then( () => {
-        if( queue.length === 0 ) {
-            callback( visited );
-        } else {
-            let nextPostId = queue.shift();
-            traversePost( nextPostId, queue, visited, callback );
+function getFavorites( userID ) {
+    return new Promise( ( resolve, reject ) => {
+        let promiseArray = [];
+        const STORY_REGEX = /<tr class='athing' id='([0-9]+)'/g;
+        for ( let page in [...Array(2).keys()] ) { // Only get two pages of favorites (60 favorites)
+            promiseArray.push( request( `https://news.ycombinator.com/favorites?id=${userID}&p=${+page + 1}` ) );
         }
+        Promise.all( promiseArray ).then( pages => {
+            let favorites = pages.map( ( page ) => {
+                let matchArr;
+                let pageFavorites = [];
+                while ( ( matchArr = STORY_REGEX.exec( page ) ) !== null ) {
+                    pageFavorites.push( matchArr[1] );
+                }
+                return pageFavorites;
+            });
+            resolve( [].concat.apply( [], favorites ) ); // Flatten favorites arrays
+        }).catch( ( error ) => reject( error ) );
+    });
+}
+
+function getItem( type, itemID ) {
+    // Accepts types of: item (story), and user
+    return new Promise( ( resolve, reject ) => {
+        request( `${API_BASE_URL}${type}/${itemID}.json` ).then( ( response ) => {
+            resolve( JSON.parse( response ) );
+        }).catch( ( error ) => reject( error ) );
+    });
+}
+
+function getTopPosts( numPosts ) {
+    return new Promise( ( resolve, reject ) => {
+        request( `${API_BASE_URL}topstories.json` ).then( ( response ) => {
+            resolve( JSON.parse( response ).slice( 0, numPosts ) );
+        }).catch( ( error ) => reject( error ) );
     });
 }
 
